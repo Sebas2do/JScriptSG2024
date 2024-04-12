232897ae0d7b34c964e7f451bc1a741df0b0b3fc
Sebastian Grisanti <sgrisanti97@gmail.com>
2024-04-12 13:17:38 -0600

    helloWorld

---
+++ HelloWorld.js
@@ -0,0 +1,9 @@
+alert("Hello World")
+
+let js = 'amazing'
+if( js === 'amazing') alert('JS is Fun')
+
+if( js === 'boring') alert('JS is Fun')
+
+let = 'broing'
+if( js === 'boring') alert('JS is Fun')
