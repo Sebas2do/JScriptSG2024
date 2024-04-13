ef1807f1727e3f72b0a6d59a05da912342748941
Sebastian Grisanti <sgrisanti97@gmail.com>
2024-04-12 18:46:48 -0600

    practicajs

--- HelloWorld.js
+++ HelloWorld.js
@@ -7,3 +7,11 @@ if( js === 'boring') alert('JS is Fun')
 
 let = 'broing'
 if( js === 'boring') alert('JS is Fun')
+
+//JavaScript is a highlevel, Object-Oriented, multi-paradigm, programming language
+
+//Highlevel :  no tengo que precuparme por muchas de las cosas para correr le codigo
+//Object-Orinted
+//Multi-paradigm
+
+//HTML (Content), CSS(Styling), JS(Program, manipular contenido, crear web apps, "Que es lo que quiero que hagan los elementos") estos tres se usan para crear un programa
---
+++ Material/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,85 @@
+/*let js = 'amazing';
+if(js==='amazing') alert('JS is fun');
+
+console.log(70 + 30 - 31);
+
+// comenting code
+
+
+//naming convention
+
+let firstName = "Jonas";
+
+let person = "miguel";
+*/
+/*
+//Data types
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+//the variable changes the type
+javaScriptIsFun = "YES";
+console.log(typeof javaScriptIsFun);
+
+//undefined
+let year;
+console.log(year);
+console.log(typeof year);
+//change the type of year
+year = 1997;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+/*
+//let , const and var
+
+let age = 30;
+//Aqui se muta la variabble para esto sirve let
+age = 31
+
+// un valor de const no se puede cambiar
+const birthYear = 1997;
+birthYear = 1990
+*/
+
+//operators
+
+const now = 2024
+const ageSebas = now - 1997;
+const agerAly = now - 1996;
+console.log(ageSebas, agerAly);
+
+console.log(ageSebas * 2, agerAly / 2);
+
+//Concat strings
+
+const firstName = 'Sebas';
+const lastName = 'Gris';
+console.log(firstName + ' ' + lastName);
+
+// template string para concaternar Strings la mejor forma para concatenar string
+
+let x = 10 + 5; // 15
+x += 10; // x = x+10
+x++;// x = x + 1
+x--; // x = x - 1
+console.log(x);
+
+
+//operadores de comparacion
+// >, <, >=, <=, ==
+console.log(ageSebas > agerAly);
+console.log(ageSebas < agerAly);
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_precedence
+
+
+
+
